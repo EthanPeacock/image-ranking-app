4694a90b041d25f76132ffec0cba0d69eda13865
Ethan Peacock <peacockethan1@gmail.com>
2025-05-02 23:08:14 +0100

    chore: formatted the files

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,17 +1,17 @@
-import { Link, Stack } from 'expo-router';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Link, Stack } from "expo-router";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function NotFoundScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <SafeAreaView>
-        <Text>This screen does not exist.</Text>
-        <Link href="/">
-          <Text>Go to home screen!</Text>
-        </Link>
-      </SafeAreaView>
-    </>
-  );
+	return (
+		<>
+			<Stack.Screen options={{ title: "Oops!" }} />
+			<SafeAreaView>
+				<Text>This screen does not exist.</Text>
+				<Link href="/">
+					<Text>Go to home screen!</Text>
+				</Link>
+			</SafeAreaView>
+		</>
+	);
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,25 @@
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+	const [loaded] = useFonts({
+		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf")
+	});
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+	if (!loaded) {
+		// Async font loading only occurs in development.
+		return null;
+	}
 
-  return (
-    <>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </>
-  );
+	return (
+		<>
+			<Stack>
+				<Stack.Screen name="index" options={{ headerShown: false }} />
+				<Stack.Screen name="+not-found" />
+			</Stack>
+			<StatusBar style="auto" />
+		</>
+	);
 }
