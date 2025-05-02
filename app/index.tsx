cfc14183a1e26ef3a4fad96c23b0beb8dbc8718d
Ethan Peacock <peacockethan1@gmail.com>
2025-05-03 00:15:46 +0100

    feat: began implementing my album page

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,40 @@
 import { Stack } from "expo-router";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
+import { Card, FAB, Text } from "react-native-paper";
+import NoAlbums from "@/components/NoAlbums";
 
 export default function MyAlbumsPage() {
+	const insets = useSafeAreaInsets();
+
 	return (
-		<View>
+		<SafeAreaView
+			style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+			edges={['left', 'right', 'bottom']}
+		>
 			<Stack.Screen options={{ title: "My Albums" }} />
-			<Text>index</Text>
-		</View>
+			
+			{/* <NoAlbums /> */}
+			
+			<View style={{ flex: 1, width: "90%" }}>
+				<Card mode="outlined" style={{ marginTop: 20 }}>
+					<Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+					<Card.Content style={{ marginTop: 10 }}>
+						<View style={{ marginBottom: 8 }}>
+							<Text variant="titleLarge">album name</Text>
+							{/* the button to manage album here... */}
+						</View>
+						<Text variant="titleSmall" style={{ marginBottom: 10 }} numberOfLines={2}>album description will certinaly go here for sure for sure</Text>
+						<Text variant="bodySmall">15 Images</Text>
+					</Card.Content>
+				</Card>
+			</View>
+
+			<FAB
+				icon="plus"
+				style={{ position: "absolute", bottom: insets.bottom + 20, right: 32 }}
+				onPress={() => console.log("hello")}
+			/>
+		</SafeAreaView>
 	);
 }
---
+++ components/NoAlbums.tsx
@@ -0,0 +1,12 @@
+import Entypo from "@expo/vector-icons/Entypo";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export default function NoAlbums() {
+	return (
+		<View style={{ marginTop: 300, alignItems: "center", justifyContent: "center" }}>
+			<Entypo name="images" size={80} color="gray" />
+			<Text variant="titleLarge" style={{ marginTop: 10 }}>No Albums</Text>
+		</View>
+	)
+}
