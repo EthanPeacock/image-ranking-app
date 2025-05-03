ac0e6fc06bfc37ee94502c7524635cad7c9d5a77
Ethan Peacock <peacockethan1@gmail.com>
2025-05-03 23:05:02 +0100

    feat: implemented manage album page UI

--- app/manage.tsx
+++ app/manage.tsx
@@ -1,11 +1,61 @@
+import ErrorPopup from "@/components/ErrorPopup";
 import { Stack } from "expo-router";
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { View } from "react-native";
+import { Button, Text, TextInput } from "react-native-paper";
 
 export default function ManageAlbumPage() {
+	const NAME_LENGTH = 50;
+	const DESCRIPTION_LENGTH = 100;
+
+	const [error, setError] = useState<boolean>(false);
+	const [name, setName] = useState<string>();
+	const [description, setDescription] = useState<string>();
+
+	const handleSubmit = () => {
+		if (!name || !description) {
+			setError(true);
+			return;
+		}
+
+		setError(false);
+		// TODO: handle update
+	};
+
 	return (
-		<View>
+		<View style={{ padding: 24, flex: 1 }}>
 			<Stack.Screen options={{ title: "Manage Album" }} />
-			<Text>manage album page</Text>
+
+			{error && <ErrorPopup message="Ensure all fields are provided." />}
+
+			<View style={{ marginBottom: 24 }}>
+				<Text variant="titleLarge" style={{ marginBottom: 12 }}>
+					Details
+				</Text>
+				<TextInput
+					mode="outlined"
+					label="Album Name"
+					maxLength={NAME_LENGTH}
+					value={name}
+					onChangeText={(value) => setName(value)}
+					right={<TextInput.Affix text={`${name?.length || 0}/${NAME_LENGTH}`} />}
+					style={{ marginBottom: 12 }}
+				/>
+				<TextInput
+					mode="outlined"
+					label="Album Description"
+					multiline={true}
+					numberOfLines={3}
+					maxLength={DESCRIPTION_LENGTH}
+					value={description}
+					onChangeText={(value) => setDescription(value)}
+					right={<TextInput.Affix text={`${description?.length || 0}/${DESCRIPTION_LENGTH}`} />}
+				/>
+			</View>
+
+			<Button mode="contained" labelStyle={{ fontSize: 16 }} onPress={handleSubmit}>
+				Update Album Details
+			</Button>
 		</View>
 	);
 }
